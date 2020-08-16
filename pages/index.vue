9a47c55bcf918f20ed86b49e1f94f491744a51ed
Jesús García <ctw@ctwhome.com>
2020-08-16 20:09:42 +0200

    feat: move to Nuxt generate

--- pages/index.vue
+++ pages/index.vue
@@ -58,7 +58,7 @@ export default {
         reverse: true, // reverse the tilt direction
         max: 25, // max tilt rotation (degrees)
         perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
-        scale: 1.2, // 2 = 200%, 1.5 = 150%, etc..
+        scale: 1, // 2 = 200%, 1.5 = 150%, etc..
         speed: 2000, // Speed of the enter/exit transition
         transition: true, // Set a transition on enter/exit.
         axis: null, // What axis should be disabled. Can be X or Y.
