356eae2614eeef764f27023cccd49b05a95abe72
Jesús García <ctw@ctwhome.com>
2020-12-04 12:55:26 +0100

    feat: netlifycms

--- pages/index.vue
+++ pages/index.vue
@@ -130,6 +130,22 @@
       <NuxtLink to="/">
         <h1 class="font-bold text-4xl">Blog Posts</h1>
       </NuxtLink>
+      <h3 class="mb-4 font-bold text-2xl uppercase text-center">Topics</h3>
+      <ul class="flex flex-wrap mb-4 text-center">
+        <li
+          v-for="tag of tags"
+          :key="tag.slug"
+          class="xs:w-full md:w-1/3 lg:flex-1 px-2 text-center"
+        >
+          <NuxtLink :to="`/blog/tag/${tag.slug}`" class="">
+            <p
+              class="font-bold text-gray-600 uppercase tracking-wider font-medium text-ss"
+            >
+              {{ tag.name }}
+            </p>
+          </NuxtLink>
+        </li>
+      </ul>
       <ul class="flex flex-wrap">
         <li
           v-for="article of articles"
@@ -158,39 +174,11 @@
           </NuxtLink>
         </li>
       </ul>
-      <h3 class="mb-4 font-bold text-2xl uppercase text-center">Topics</h3>
-      <ul class="flex flex-wrap mb-4 text-center">
-        <li
-          v-for="tag of tags"
-          :key="tag.slug"
-          class="xs:w-full md:w-1/3 lg:flex-1 px-2 text-center"
-        >
-          <NuxtLink :to="`/blog/tag/${tag.slug}`" class="">
-            <p
-              class="font-bold text-gray-600 uppercase tracking-wider font-medium text-ss"
-            >
-              {{ tag.name }}
-            </p>
-          </NuxtLink>
-        </li>
-      </ul>
+
       <footer class="flex justify-center border-gray-500 border-t-2">
         <p class="mt-4">
-          Created by
-          <a
-            href="https://twitter.com/debs_obrien"
-            class="font-bold hover:underline"
-          >
-            Debbie O'Brien
-          </a>
-          at NuxtJS. See the
-          <a
-            href="https://nuxtjs.org/blog/creating-blog-with-nuxt-content"
-            class="font-bold hover:underline"
-          >
-            tutorial
-          </a>
-          for how to build it.
+          Perfonmance in the NUMBER 1 UX rule. Did you see how fast this website
+          load?
         </p>
       </footer>
     </div>
