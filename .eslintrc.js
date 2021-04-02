12aabf5280ac0686e6496e2413a765e594fea5fb
Jesús García <ctw@ctwhome.com>
2021-04-02 21:04:23 +0200

    minor: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,13 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: ['prettier'],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended'],
+  plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {}
 }
