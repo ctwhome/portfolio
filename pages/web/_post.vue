033d1906d686f8882138347bb0d757ea8e438dfb
Jesús García <ctw@ctwhome.com>
2021-08-07 08:58:16 +0200

    feat: first post!

--- pages/web/_post.vue
+++ pages/web/_post.vue
@@ -53,6 +53,7 @@ export default {
     }
   },
   methods: {
+
     formatDate (date) {
       const options = { year: 'numeric', month: 'long', day: 'numeric' }
       return new Date(date).toLocaleDateString('en', options)
