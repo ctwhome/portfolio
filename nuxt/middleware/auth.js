f70bb3221a8d97037ac2abc74e32aa37ee36c556
Jesús García <ctw@ctwhome.com>
2023-09-30 20:10:55 +0200

    remove nuxt

--- nuxt/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-// import { isLoggedIn } from '~/api/auth'
-const isLoggedIn = true
-export default function ({ redirect }) {
-  if (!isLoggedIn.value) {
-    console.error('Protected Route')
-    return redirect('/')
-  }
-}
