e4c9ea48640e2872e160e996bd69463436e58699
Jesús García <ctw@ctwhome.com>
2023-09-30 14:01:08 +0200

    migration to sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import '$lib/assets/css/app.css'
+    import Header from '$lib/components/Header.svelte'
+</script>
+
+
+
+<div class="flex flex-col h-screen">
+    <!-- <header-main /> -->
+    <Header/>
+<div class="flex-1">  <!-- todo: this doens't work -->
+    <slot>
+        <!-- Content here -->
+    </slot>
+</div>
+</div>
