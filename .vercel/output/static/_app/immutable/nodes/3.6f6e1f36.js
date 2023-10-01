0b526f1fd8bcefce88df2ee0486319ce344b28eb
Jesús García <ctw@ctwhome.com>
2023-10-01 21:05:25 +0200

    clean up

--- .vercel/output/static/_app/immutable/nodes/3.6f6e1f36.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.a0fef3da.js";import{S as t,i as e}from"../chunks/index.88c54976.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
