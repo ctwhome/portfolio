f7707af0d4fba19d78fc466426add057017be5fe
Jesús García <ctw@ctwhome.com>
2023-10-01 13:04:58 +0200

    remove nuxt

--- nuxt/layouts/default.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="flex flex-col h-screen">
-    <header-main />
-    <Nuxt class="flex-1" />
-  </div>
-</template>
--- nuxt/utils/flattenTags.js
+++
@@ -1,7 +0,0 @@
-
-export default function (tags) {
-  const mergedTags = []
-  tags.map(article => mergedTags.push(article.tags))
-  // Flatten array and remove duplicates
-  return [...new Set(mergedTags.flat())]
-}
