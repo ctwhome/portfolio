ab6cdc86d9876b24d854e2a0ce66fbfc1b132242
Jesús García <ctw@ctwhome.com>
2023-10-04 21:11:32 +0200

    add year of publication to the work list

--- src/routes/work/+page.server.ts
+++ src/routes/work/+page.server.ts
@@ -1,3 +1,30 @@
+function groupPostsByYear(posts) {
+	// Sort the posts by their Unix timestamp in descending order
+	posts.sort((a, b) => b.post.unix_timestamp - a.post.unix_timestamp);
+
+	// Initialize an empty object to store the posts grouped by year
+	const postsByYear = {};
+
+	// Loop through each post
+	posts.forEach(({ post }) => {
+		// Convert the Unix timestamp to a JavaScript Date object
+		const date = new Date(post.unix_timestamp * 1000);
+
+		// Extract the year from the Date object
+		const year = date.getFullYear();
+
+		// Initialize the year property if it doesn't exist yet
+		if (!postsByYear[year]) {
+			postsByYear[year] = [];
+		}
+
+		// Push the post into the appropriate year property
+		postsByYear[year].push(post);
+	});
+
+	return postsByYear;
+}
+
 export async function load({ params }) {
 	let posts = [];
 
@@ -14,7 +41,7 @@ export async function load({ params }) {
 		console.error('Failed to fetch posts');
 	}
 
-	return { posts };
+	return { posts: groupPostsByYear(posts), count: posts.length };
 }
 
 // let posts = [];
--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -2,7 +2,7 @@
 	// import { lazyLoad } from '$lib/actions/lazy-load.js';
 	import { onMount } from 'svelte';
 	export let data;
-	const { posts } = data;
+	const { posts, count } = data;
 
 	onMount(() => {
 		// Get all image elements on the page
@@ -17,57 +17,53 @@
 	});
 </script>
 
-<!-- <pre>{JSON.stringify(data, null, 2)}</pre> -->
+<!-- <pre>{JSON.stringify(posts, null, 2)}</pre> -->
 <!-- <pre>{JSON.stringify(posts[3].post, null, 2)}</pre> -->
 
 <main class="mx-auto max-w-[900px] px-4">
-	<h1 class="text-2xl sm:text-4xl font-bold px-2">Latest Work ({data?.posts?.length})</h1>
-
-	{#if data?.posts?.length === 0}
+	<h1 class="text-2xl sm:text-4xl font-bold">
+		Latest Work ({count})
+	</h1>
+	{#if !data}
 		<p>Loading...</p>
-	{:else if data?.posts?.length > 0}
-		<ul class="grid grid-cols-2 sm:grid-cols-1 gap-4 sm:gap-5 mt-10">
-			{#each data.posts as { post }}
-				<a
-					data-sveltekit-preload-data="hover"
-					href={'/' + post.categories[0].name + '/' + post.slug}
-					class="flex flex-col sm:flex-row gap-4 rounded hover:bg-base-200 transition bg-base-200 sm:bg-inherit sm:p-4"
-				>
-					<div class="flex-none">
-						{#if post.media_url}
-							<img
-								class="w-full sm:w-[150px] aspect-[5/3] object-cover rounded rounded-b-none sm:rounded-b-md"
-								src={post.media_url}
-								alt={post.post_title}
-							/>
-							<!-- use:lazyLoad={post.media_url} -->
-						{/if}
-					</div>
-					<div class="px-3 pb-3">
-						<h2 class="text-ld line-clamp-3 sm:text-2xl font-bold">{post.post_title}</h2>
-
-						<div class="prose line-clamp-3 mt-2 leading-5 sm:leading-auto text-sm">
-							{@html post.excerpt}
+	{:else}
+		{#each Object.keys(posts).sort().reverse() as year}
+			<h2 class="text-xl font-bold mt-8 opacity-80">{year}</h2>
+			<ul class="grid grid-cols-2 sm:grid-cols-1 gap-4 sm:gap-5 mt-10">
+				{#each posts[year] as post}
+					<a
+						data-sveltekit-preload-data="hover"
+						href={'/' + post.categories[0].name + '/' + post.slug}
+						class="flex flex-col sm:flex-row gap-4 rounded hover:bg-base-200 transition bg-base-200 sm:bg-inherit sm:p-4"
+					>
+						<div class="flex-none">
+							{#if post.media_url}
+								<img
+									class="w-full sm:w-[150px] aspect-[5/3] object-cover rounded rounded-b-none sm:rounded-b-md"
+									src={post.media_url}
+									alt={post.post_title}
+								/>
+							{/if}
 						</div>
+						<div class="px-3 pb-3">
+							<h2 class="text-ld line-clamp-3 sm:text-2xl font-bold">{post.post_title}</h2>
 
-						<div class="flex gap-3 mt-2 opacity-40 text-sm">
-							<div class="flex flex-wrap gap-3">
-								{#each post.categories as categorie}
-									<div>{categorie.name}</div>
-								{/each}
+							<div class="prose line-clamp-3 mt-2 leading-5 sm:leading-auto text-sm">
+								{@html post.excerpt}
 							</div>
-							<!-- |
-							<div class="flex flex-wrap gap-3">
-								{#each post.tags as tag}
-									<div>{tag.name}</div>
-								{/each}
-							</div> -->
+
+							<div class="flex gap-3 mt-2 opacity-40 text-sm">
+								<div class="flex flex-wrap gap-3">
+									{#each post.categories as categorie}
+										<div>{categorie.name}</div>
+									{/each}
+								</div>
+							</div>
 						</div>
-						<!-- <button class="btn btn-ghost hover:btn-primary">View Post</button> -->
-					</div>
-				</a>
-			{/each}
-		</ul>
+					</a>
+				{/each}
+			</ul>
+		{/each}
 	{/if}
 </main>
 <!--
