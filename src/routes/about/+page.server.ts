293402728d162098fde74b6001d66881b9a1aa4b
Jesús García <ctw@ctwhome.com>
2023-10-06 23:25:06 +0200

    add seo to about page

--- src/routes/about/+page.server.ts
+++ src/routes/about/+page.server.ts
@@ -1,9 +1,13 @@
-export async function load({}) {
+import { replaceURL } from '$lib/utils/repace-url-for-seo.js';
+
+export async function load() {
 	// fetch page from wordpress json api
 	const res = await fetch(`https://portfolio.ctwhome.com/wp-json/wp/v2/pages?slug=about}`);
-	const page = await res.json();
+	const [page] = await res.json();
+	const yoast_head = replaceURL(page.yoast_head);
 
 	return {
-		page
+		page,
+		yoast_head
 	};
 }
