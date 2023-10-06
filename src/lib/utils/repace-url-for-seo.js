ad992d87fe49e1172ecd5bae7fa5711e32c3fdde
Jesús García <ctw@ctwhome.com>
2023-10-06 23:07:01 +0200

    replace noindex

--- src/lib/utils/repace-url-for-seo.js
+++ src/lib/utils/repace-url-for-seo.js
@@ -2,5 +2,7 @@
 // Description: Replace the url of the image with the url of the category
 export const replaceURL = (inputString, category) => {
 	const regex = /(https:\/\/portfolio\.ctwhome\.com\/)(?!wp-content)/g;
-	return inputString.replace(regex, (match) => 'https://ctwhome.com/');
+	return inputString
+		.replace(regex, (match) => 'https://ctwhome.com/') // replace the url
+		.replace(/content="noindex/g, 'content="index'); // replace noindex with index
 };
