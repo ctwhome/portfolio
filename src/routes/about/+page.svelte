2d1257d894eb282f23c3b5d5a580c48f8a752c0f
Jesús García <ctw@ctwhome.com>
2023-10-06 20:53:54 +0200

    add seo to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,15 @@
 <script>
 	export let data;
-	const [page] = data.page;
+	const {page, yoast_head} = data;
 </script>
 
 <!-- <pre>{JSON.stringify(page, null, 2)}</pre> -->
 
+<svelte:head>
+	{#if yoast_head}
+		{@html yoast_head}
+	{/if}
+</svelte:head>
 <!-- if page show the content and the title -->
 <div class="prose mx-auto px-4 sm:px-0">
 	{#if page}
