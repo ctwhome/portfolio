1dc1e7e7408b382579d243f9d52ccc843701ca02
Jesús García <ctw@ctwhome.com>
2023-10-07 22:15:16 +0200

    add profile photo

--- src/routes/[category]/[publication]/+page.svelte
+++ src/routes/[category]/[publication]/+page.svelte
@@ -1,10 +1,10 @@
 <script>
 	import VanillaTilt from 'vanilla-tilt';
 	import { onMount, onDestroy } from 'svelte';
-	import { page } from '$app/stores';
+	import ctwhomeProfile from '$lib/assets/images/ctwhome-profile.webp';
 
 	export let data;
-	const { post, media, date, tags, categories, nextPost, previousPost, yoast_head } = data;
+	const { post, media, date, tags, categories, next_prev, yoast_head } = data;
 
 	let large = media?.media_details?.sizes?.large?.source_url || media?.source_url;
 	let title = post?.title?.rendered;
@@ -49,15 +49,16 @@
 		{@html yoast_head}
 	{/if}
 </svelte:head>
-
 <div class="prose mx-auto mt-6 px-4 sm:px-0">
 	{#if post}
-		<h1>{@html title}</h1>
+		<h1 class="mb-0">{@html title}</h1>
+
 		<p class="text-sm">
-			{date} -
 			{#each categories as category}
 				<span class="mx-1">{category.name}</span>
 			{/each}
+			-
+			{date}
 
 			{#if tags.length > 0}
 				|
@@ -67,37 +68,77 @@
 				<span class="mx-1">{tag.name}</span>
 			{/each}
 		</p>
-		<!-- each categories show catory.name -->
+		<!-- Profile picture -->
+		<div class="flex items-center gap-3">
+			<img
+				width="60"
+				height="60"
+				class="mask mask-hexagon my-2"
+				src={ctwhomeProfile}
+				alt="J. Gonzalez Ctwhome profile picture"
+			/>
+			<div>
+				<div class="font-bold">J. Gonzalez - Ctw</div>
+				<div>Product Designer & Research Software Engineer</div>
+			</div>
+		</div>
 
 		{#if media}
 			<img
 				src={large}
 				alt={title}
 				bind:this={tiltCover}
-				class="w-full aspect-[5/3] object-cover rounded sm:rounded-lg"
+				class="w-full aspect-[5/3] object-cover rounded sm:rounded-lg mb-10"
 			/>
 		{/if}
+
 		{@html content}
 	{/if}
-
-	<div class="grid grid-cols-2 gap-4 w-full">
-		<div class="">
-			{#if previousPost}
+</div>
+{#if next_prev}
+	<div
+		class="grid grid-cols-2 gap-6 sm:gap-6 w-full container mx-auto mt-24 mb-10 min-h-52 p-4 items-center bg-base-200 bg-opacity-30 rounded-lg"
+	>
+		{#if next_prev.next}
+			<div>
+				<!-- <pre>{JSON.stringify(next_prev.next, null, 2)}</pre> -->
 				<a
-					data-sveltekit-preload-data="hover"
+					data-sveltekit-preload-data=""
 					data-sveltekit-reload
-					href={`/work/${previousPost.slug}`}
+					href={`/${next_prev.next.categories[0]}/${next_prev.next.slug}`}
 				>
-					←
-					{previousPost.title.rendered}</a
-				>
-			{/if}
-		</div>
+					<div class="text-2xl">Prev</div>
+					<img
+						width="300"
+						height=""
+						class="aspect-[5/3] object-cover rounded"
+						src={next_prev.next.thumbnail}
+						alt=""
+					/>
+					<div class="font-bold mt-4 block max-w-[300px]">{next_prev.next.title}</div>
+				</a>
+			</div>
+		{/if}
 
-		{#if nextPost}
-			<a data-sveltekit-preload-data="hover" data-sveltekit-reload href={`/work/${nextPost.slug}`}
-				>→ {nextPost.title.rendered} {nextPost.slug}</a
-			>
+		{#if next_prev.prev}
+			<div>
+				<!-- <pre>{JSON.stringify(next_prev.prev, null, 2)}</pre> -->
+				<a
+					data-sveltekit-preload-data=""
+					data-sveltekit-reload
+					href={`/${next_prev.prev.categories[0]}/${next_prev.prev.slug}`}
+				>
+					<div class="text-2xl">Next</div>
+					<img
+						width="300"
+						height="180"
+						class="aspect-[5/3] object-cover rounded"
+						src={next_prev.prev.thumbnail}
+						alt=""
+					/>
+					<div class="font-bold mt-4 block">{next_prev.prev.title}</div>
+				</a>
+			</div>
 		{/if}
 	</div>
-</div>
+{/if}
