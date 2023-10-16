6bc844dcc834f75bceb6367ae235a8b23b7d9b41
Jesús García <ctw@ctwhome.com>
2023-10-16 20:51:34 +0200

    Add improve this page button

--- src/routes/[category]/[publication]/+page.svelte
+++ src/routes/[category]/[publication]/+page.svelte
@@ -145,3 +145,20 @@
 		{/if}
 	</div>
 {/if}
+
+<div class="container mx-auto">
+	<a
+		href={`https://portfolio.ctwhome.com/wp-admin/post.php?post=&{post.id}&action=edit`}
+		rel="nofollow"
+		title="Edit post"
+		class="flex gap-2 items-center no-underline hover:underline hover:decoration-primary opacity-70"
+	>
+		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+			><path
+				fill="currentColor"
+				d="m19.3 8.925l-4.25-4.2l1.4-1.4q.575-.575 1.413-.575t1.412.575l1.4 1.4q.575.575.6 1.388t-.55 1.387L19.3 8.925ZM4 21q-.425 0-.713-.288T3 20v-2.825q0-.2.075-.388t.225-.337l10.3-10.3l4.25 4.25l-10.3 10.3q-.15.15-.337.225T6.825 21H4Z"
+			/></svg
+		>
+		Improve this page
+	</a>
+</div>
