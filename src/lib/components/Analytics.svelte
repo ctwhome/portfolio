d37f689a3dca5175df77f25157396618e7301144
Jesús García <ctw@ctwhome.com>
2023-10-23 16:13:55 +0200

    gtag

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -4,7 +4,7 @@
 	const MEASUREMENT_ID = 'G-DHQ2TX1XM1';
 	$: {
 		if (typeof gtag !== 'undefined') {
-			gtag('config', MEASUREMENT_ID, {
+			gtag('config', 'G-DHQ2TX1XM1', {
 				page_title: document.title,
 				page_path: $page.url.pathname
 			});
@@ -13,7 +13,7 @@
 </script>
 
 <svelte:head>
-	<script async src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}>
+	<script async src={`https://www.googletagmanager.com/gtag/js?id=G-DHQ2TX1XM1`}>
 	</script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
@@ -23,6 +23,6 @@
 		}
 
 		gtag('js', new Date());
-		gtag('config', MEASUREMENT_ID);
+		gtag('config', 'G-DHQ2TX1XM1');
 	</script>
 </svelte:head>
