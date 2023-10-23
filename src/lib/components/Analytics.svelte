d3db4e08db78b17202e3de8189c2369d8f75834f
Jesús García <ctw@ctwhome.com>
2023-10-23 16:07:24 +0200

    Add google Analytics

---
+++ src/lib/components/Analytics.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	const MEASUREMENT_ID = 'G-DHQ2TX1XM1';
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', MEASUREMENT_ID, {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script async src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}>
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', MEASUREMENT_ID);
+	</script>
+</svelte:head>
