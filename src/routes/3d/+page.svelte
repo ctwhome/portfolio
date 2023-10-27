a61376bd9e23c07b95282b2801e3c18c5ce3496b
Jesús García <ctw@ctwhome.com>
2023-10-27 12:37:32 +0200

    Add 3D experiments

--- src/routes/3d/+page.svelte
+++ src/routes/3d/+page.svelte
@@ -1,7 +1,13 @@
-<script>
-	import ThreeJS from './three.svelte';
-</script>
-
-This is a ThreeJS page
-
-<ThreeJS />
+<div class="container mx-auto">
+	<ul>
+		<li><a class="hover:text-primary" href="3d/complete">Complete Example</a></li>
+		<li>
+			<a
+				class="hover:text-primary"
+				data-sveltekit-preload-data=""
+				data-sveltekit-reload
+				href="/nebula/index.html">Nebula</a
+			>
+		</li>
+	</ul>
+</div>
