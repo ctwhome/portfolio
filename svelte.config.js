b979609c008425d5abcd8b90d209f160f4e5e089
Jesús García <ctw@ctwhome.com>
2024-04-03 20:51:02 +0200

    update to sveltekit 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
 // import adapter from '@sveltejs/adapter-vercel';
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { importAssets } from 'svelte-preprocess-import-assets';
 
 /** @type {import('@sveltejs/kit').Config} */
