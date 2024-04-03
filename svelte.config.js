ca9906a1d858dce5b4e424790f2a05fce096ecd4
Jesús García <ctw@ctwhome.com>
2024-04-03 21:12:52 +0200

    Update Vite and Svelte dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,16 @@
 // import adapter from '@sveltejs/adapter-vercel';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { importAssets } from 'svelte-preprocess-import-assets';
+// import { importAssets } from 'svelte-preprocess-import-assets';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), importAssets()],
+	preprocess: [
+		vitePreprocess(),
+		// importAssets()
+	],
 
 	kit: {
 		// https://kit.svelte.dev/docs/adapter-static
