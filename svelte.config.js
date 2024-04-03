353c53a235b0754e056a181208a9989256a5f79f
Jesús García <ctw@ctwhome.com>
2024-04-03 21:10:12 +0200

    Update Svelte configuration and package.json

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 // import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-vercel';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-// import { importAssets } from 'svelte-preprocess-import-assets';
+import { importAssets } from 'svelte-preprocess-import-assets';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +10,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [
 		vitePreprocess(),
-		// importAssets()
+		importAssets()
 	],
 
 	kit: {
