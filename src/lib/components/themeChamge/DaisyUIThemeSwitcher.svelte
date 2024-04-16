b8ead1c7ccfa3a8a5c168d62fdbdac49bf4c7698
Jesús García <ctw@ctwhome.com>
2024-04-16 12:35:20 +0200

    Update themeChamge component to use "Change theme" instead of "change-theme"

--- src/lib/components/themeChamge/DaisyUIThemeSwitcher.svelte
+++ src/lib/components/themeChamge/DaisyUIThemeSwitcher.svelte
@@ -28,7 +28,7 @@
 				d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
 			/>
 		</svg>
-		<span class="hidden md:inline">Change theme</span>
+		<span class="hidden">Change theme</span>
 		<svg
 			width="12px"
 			height="12px"
@@ -40,7 +40,7 @@
 		</svg>
 	</div>
 	<div
-		class="dropdown-content bg-base-200 text-base-content rounded-t-box rounded-b-box top-px max-h-96 h-[70vh] w-52 overflow-y-auto shadow-2xl"
+		class="z-10 dropdown-content bg-base-200 text-base-content rounded-t-box rounded-b-box top-px max-h-96 h-[70vh] w-52 overflow-y-auto shadow-2xl"
 	>
 		<div class="grid grid-cols-1 gap-3 p-3" tabIndex="0">
 			{#each themes as theme}
