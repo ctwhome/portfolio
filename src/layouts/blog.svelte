cb773c4ce9321a4c8b53a52e26d23b691bafe2d4
Jesús García <ctw@ctwhome.com>
2024-04-19 20:07:05 +0200

    wip

--- src/layouts/blog.svelte
+++ src/layouts/blog.svelte
@@ -37,32 +37,32 @@
 
 	// generated open-graph image for sharing on social media.
 	// see https://og-image.vercel.app/ for more options.
-	const ogImage = `https://og-image.vercel.app/**${encodeURIComponent(
-		data.post.title
-	)}**?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-color-logo.svg`;
+	// const ogImage = `https://og-image.vercel.app/**${encodeURIComponent(
+	// 	data.post.title
+	// )}**?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-color-logo.svg`;
 </script>
 
 <!-- TODO WIP get og data in place -->
-<svelte:head>
+<!-- <svelte:head>
 	<title>{data.post.title} - {name}</title>
 	<meta name="description" content={data.post.preview.text} />
 	<meta name="author" content={name} />
 
-	<!-- Facebook Meta Tags -->
+	Facebook Meta Tags
 	<meta property="og:url" content={url} />
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content={data.post.title} />
 	<meta property="og:description" content={data.post.preview.text} />
 	<meta property="og:image" content={ogImage} />
 
-	<!-- Twitter Meta Tags -->
+	Twitter Meta Tags
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta property="twitter:domain" content={website} />
 	<meta property="twitter:url" content={url} />
 	<meta name="twitter:title" content={data.post.title} />
 	<meta name="twitter:description" content={data.post.preview.text} />
 	<meta name="twitter:image" content={ogImage} />
-</svelte:head>
+</svelte:head> -->
 
 <div class="mx-auto prose py-10 px-3">
 	<h1 class="text-4xl font-bold mb-5">
