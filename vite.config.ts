9cecc35517001a64e10260a19bc0a85e051e61c6
Jesús García <ctw@ctwhome.com>
2024-04-22 21:55:29 +0200

    almost there!

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,19 @@ import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import Icons from 'unplugin-icons/vite'
 
+import VitePluginRestart from 'vite-plugin-restart';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+
 // import copyImagesPlugin from './vitePlugins/vite-plugin-copy-images';
 
 export default defineConfig({
 	plugins: [
 		enhancedImages(),
+
+		VitePluginRestart({ restart: ['./content/**'] }),
+		viteStaticCopy({ targets: [{ src: './content/*', dest: './content/' }] }),
+
 		sveltekit(),
 		Icons({
 			compiler: 'svelte',
