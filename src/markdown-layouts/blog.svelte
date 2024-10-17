4c087785887dac11c204bade15b59f9be1eb6931
Jesús García <ctw@ctwhome.com>
2024-10-17 21:19:00 +0200

    gallery improvements

--- src/markdown-layouts/blog.svelte
+++ src/markdown-layouts/blog.svelte
@@ -1,9 +1,10 @@
-<script>
+<script lang="ts">
 	import Gallery from '$lib/components/Gallery.svelte';
 	import { onMount } from 'svelte';
 
-	let images = [];
-	let videos = [];
+	let images: string[] = [];
+	let videos: string[] = [];
+	let galleryComponent: Gallery;
 
 	onMount(() => {
 		// Find all images and videos in the content
@@ -12,21 +13,39 @@
 			images = Array.from(content.querySelectorAll('img')).map((img) => img.src);
 			videos = Array.from(content.querySelectorAll('video')).map((video) => video.src);
 
-			// Remove original images and videos
-			// content.querySelectorAll('img, video').forEach((el) => el.remove());
+			// Add click event listeners to images
+			content.querySelectorAll('img').forEach((img, index) => {
+				img.style.cursor = 'pointer';
+				img.addEventListener('click', (e) => {
+					e.preventDefault();
+					galleryComponent.openGallery(index);
+				});
+			});
+
+			// Add click event listeners to videos
+			content.querySelectorAll('video').forEach((video, index) => {
+				video.style.cursor = 'pointer';
+				video.addEventListener('click', (e) => {
+					e.preventDefault();
+					galleryComponent.openGallery(images.length + index);
+				});
+			});
 		}
 	});
 </script>
 
 <div class="prose mx-auto w-full overflow-auto break-words px-3 sm:prose-lg">
 	<slot />
-	{#if images.length > 0 || videos.length > 0}
-		<Gallery {images} {videos} />
-	{/if}
 </div>
 
+{#if images.length > 0 || videos.length > 0}
+	<Gallery bind:this={galleryComponent} {images} {videos} />
+{/if}
+
 <style>
-	/* :global(.prose img, .prose video) {
-		display: none;
-	}*/
+	:global(.prose img, .prose video) {
+		display: inline-block;
+		max-width: 100%;
+		height: auto;
+	}
 </style>
