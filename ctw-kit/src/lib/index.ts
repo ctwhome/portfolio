17a76cbb0a7d3fb6cbea79a2a8c62056cb4cc503
Jesús García <ctw@ctwhome.com>
2025-01-05 06:56:16 +0100

    chore: button closure in portfolio markdown, correct Carousel component path, and update package dependencies

--- ctw-kit/src/lib/index.ts
+++ ctw-kit/src/lib/index.ts
@@ -1,6 +1,6 @@
 export { default as SEO } from './components/SEO/SEO.svelte';
 export { default as Hello } from './components/Hello/Hello.svelte';
-export { default as Carousel } from './components/Carrousel/Carousel.svelte';
+export { default as Carousel } from './components/Carousel/Carousel.svelte';
 export { default as TiltContent } from './components/TiltContent/TiltContent.svelte';
 export { default as ThemeChange } from './components/ThemeChange/ThemeChange.svelte';
 export { default as SendEmail } from './components/Emails/SendEmail.svelte';
