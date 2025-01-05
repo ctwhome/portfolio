252a8bfbcc379dfa0087eebfc3be0b440d5ea85f
Jesús García <ctw@ctwhome.com>
2025-01-05 07:16:46 +0100

    moving portfolio website to vercel

--- portfolio/svelte.config.js
+++ portfolio/svelte.config.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from './mdsvex.config.js'
