668b59f4e424f5fe675441b6471495aa3a4a3da8
Jesús García <ctw@ctwhome.com>
2025-01-05 07:25:42 +0100

    chore: update README for ctw-kit integration and add dependency in package.json

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -2,12 +2,19 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import Icons from 'unplugin-icons/vite'
-
 import VitePluginRestart from 'vite-plugin-restart';
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path';
 
-
-export default defineConfig({
+// @ts-ignore
+export default defineConfig(({ mode }) => ({
+	resolve: {
+		alias: {
+			'ctw-kit': mode === 'development'
+				? path.resolve(__dirname, '../ctw-kit/src/lib')
+				: 'ctw-kit'
+		}
+	},
 	plugins: [
 		enhancedImages(),
 
@@ -23,4 +30,4 @@ export default defineConfig({
 	// optimizeDeps: {
 	// 	disabled: true,
 	// },
-});
+}));
