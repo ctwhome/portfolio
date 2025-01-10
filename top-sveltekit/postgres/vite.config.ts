5a634c63f311fc7f15299b2f319424275520ca48
Jesús García <ctw@ctwhome.com>
2025-01-10 15:34:31 +0100

    feat: update environment configuration for database connections and enhance local development display

--- top-sveltekit/postgres/vite.config.ts
+++ top-sveltekit/postgres/vite.config.ts
@@ -1,25 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import Icons from 'unplugin-icons/vite'
-
 import VitePluginRestart from 'vite-plugin-restart';
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-
-export default defineConfig({
+const config = {
 	plugins: [
-		enhancedImages(),
-
-		VitePluginRestart({ restart: ['./content/**'] }),
-		viteStaticCopy({ targets: [{ src: './src/lib/content/*', dest: './content/' }] }),
-
 		sveltekit(),
 		Icons({
 			compiler: 'svelte',
 		}),
+		VitePluginRestart({ restart: ['./content/**'] }) as any,
+		viteStaticCopy({ targets: [{ src: './src/lib/content/*', dest: './content/' }] }) as any,
 	],
+	server: {
+		// Disable CORS locally to avoid CORS issues with the API in production
+		cors: false
+	},
 	// optimizeDeps: {
 	// 	disabled: true,
 	// },
-});
+};
+
+
+export default defineConfig(config);
