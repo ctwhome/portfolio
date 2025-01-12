a35b2894cb9e7f14d84e4f9807e61687e4e4e4c3
Ctw <ctw@ctwhome.com>
2025-01-12 20:34:24 +0100

    move top sveltekit to its own repo

--- top-sveltekit/postgres-rls/src/routes/contact/+page.server.ts
+++
@@ -1,9 +0,0 @@
-export async function load({}) {
-	// fetch page from wordpress json api
-	const res = await fetch(`https://portfolio.ctwhome.com/wp-json/wp/v2/pages?slug=pricing}`);
-	const page = await res.json();
-
-	return {
-		page
-	};
-}
